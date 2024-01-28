6502a9447ed9d7517ba142d4d078747ac747a511
ayush <ankithelpadi143ayush@gmail.com>
2024-01-28 10:39:32 +0530

    added contact

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const connectToMongoose = require("./db");
-var cors= require('cors')
+var cors = require("cors");
 connectToMongoose();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
-app.use("/api/auth", require("./routes/auth")); 
+app.use("/api/auth", require("./routes/auth"));
 app.use("/api/auth", require("./routes/contact"));
 
 app.listen(port, () => {
